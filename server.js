0ae333dd6296e78eced80c4c49cba96dd2e66123
Sascha Monteiro <sascha.monteiro@gmail.com>
2017-05-16 08:41:27 +0000

    updated challenge

--- server.js
+++ server.js
@@ -2,22 +2,35 @@ var express = require('express')
 var app = express()
 var mongodb = require('mongodb')
 var autoIncrement = require("mongodb-autoincrement")
-
+var dns = require('dns')
+var urlx = require('url')
+var bodyparser = require('body-parser')
 var MongoClient = mongodb.MongoClient
 var port = process.env.PORT || 8080
 var mongodburl = process.env.MONGOLAB_URI
 var collectionName = 'urlshort'
 
-app.get('/new/:url(*)', function(req, res) {
-  console.log('url', req.params.url)
-  MongoClient.connect(mongodburl, function(err, db) {
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(express.static('public'))
+app.post('/api/shorturl/new', (req, res) => {
+  var u = req.body.url
+  console.log('url', u)
+  var parsedUrl = urlx.parse(u).hostname
+  console.log('host', parsedUrl)
+  if(parsedUrl !== null){
+  dns.lookup(parsedUrl, (err2) => {
+    if(err2){
+      console.log('error', err2)
+      res.send(JSON.stringify({"error":"invalid URL"}))
+    }else{
+       MongoClient.connect(mongodburl, (err, db) => {
     if (err) {
       console.log('Unable to connect to the mongoDB server [' + mongodburl + ']. Error:', err)
     }
     else {
       console.log('Connection established to', mongodburl)
 
-      autoIncrement.getNextSequence(db, collectionName, function(err, autoIndex) {
+      autoIncrement.getNextSequence(db, collectionName, (err, autoIndex) => {
         if (err) {
           console.log('error', err)
         }
@@ -25,7 +38,7 @@ app.get('/new/:url(*)', function(req, res) {
           var collection = db.collection(collectionName)
           collection.insert({
             _id: autoIndex,
-            url: req.params.url
+            url: u
           }, function(err, records) {
             if (err) {
               console.log('error', err)
@@ -34,8 +47,8 @@ app.get('/new/:url(*)', function(req, res) {
               console.log(records)
               console.log("Record added as " + records.insertedIds[0])
               var r = {
-                original_url: req.params.url,
-                short_url: req.header('x-forwarded-proto') + "://" + req.header('host') + "/" + records.insertedIds[0]
+                original_url: u,
+                short_url: req.header('x-forwarded-proto') + "://" + req.header('host') + "/api/shorturl/" + records.insertedIds[0]
               }
               res.send(JSON.stringify(r))
             }
@@ -44,13 +57,19 @@ app.get('/new/:url(*)', function(req, res) {
         }
       })
     }
+   
+    
   })
+    }
+  })
+}
+  
 })
-app.get('/:urlid', function(req, res) {
+app.get('/api/shorturl/:urlid', (req, res) => {
   var urlid = parseInt(req.params.urlid, 10)
   console.log('urlid', urlid)
   if (!isNaN(urlid)) {
-    MongoClient.connect(mongodburl, function(err, db) {
+    MongoClient.connect(mongodburl, (err, db) => {
       if (err) {
         console.log('Unable to connect to the mongoDB server [' + mongodburl + ']. Error:', err)
       }
@@ -58,11 +77,12 @@ app.get('/:urlid', function(req, res) {
         var collection = db.collection(collectionName)
         collection.findOne({
           '_id': urlid
-        }, function(err, d) {
+        }, (err, d) => {
           if (err) {
             console.log('error', err)
           }
           else {
+            console.log('redirecting to ', d.url)
             res.writeHead(301, {
               Location: d.url
             });
@@ -75,6 +95,6 @@ app.get('/:urlid', function(req, res) {
   }
 
 })
-app.listen(port, function() {
+app.listen(port, () => {
   console.log('Example app listening on port ' + port)
 })
